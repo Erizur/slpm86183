c493aafdb5605df145b503c1ecaad42141f40025
AM_Erizur <sajoudks@outlook.com>
2025-11-10 02:13:27 -0500

    nooke

--- diff_settings.py
+++
@@ -1,6 +0,0 @@
-def apply(config, args):
-    config["arch"] = "mipsel"
-    config['baseimg'] = f'iso/SLPM_861.83'
-    config['myimg'] = f'build/SLPM_861.83'
-    config['mapfile'] = f'build/SLPM_861.83.map'
-    config['source_directories'] = [f'src/SLPM_861.83', 'include', f'asm/SLPM_861.83']
